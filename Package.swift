fe57edecae16b850a33ff23cac1cd942f026f96c
wangnov <1694546283@qq.com>
2025-12-12 14:49:43 +0800

    build(spm): 降低 macOS 最低部署版本到 14

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "Airis",
     platforms: [
-        .macOS(.v15)
+        .macOS(.v14)
     ],
     products: [
         .executable(name: "airis", targets: ["Airis"])
